57738a189cb87a54f686530cb03d9e17c67b2b73
Artyom Pionov <pionovartem11@gmail.com>
2023-03-09 20:12:16 +0000

    Second Commit

--- src/Zoo.java
+++ src/Zoo.java
@@ -1,16 +1,12 @@
 public class Zoo {
     Animal[] animals;
     public Zoo(){
-        // в этой строке мы еще не создаем животных
         animals = new Animal[3];
-        // наполняем массив конкретными животными
         animals[0] = new Dog();
         animals[1] = new Cat();
         animals[2] = new Reptile();
     }
     public void print(){
-        // Используем цикл!
-        // Каждое животное выводится по-своему!
         for( Animal a: animals){
             a.print();
         }
